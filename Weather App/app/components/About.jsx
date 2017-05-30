b63b3570288c714d3490399c87d61dec3849c4ca
Matrixmdmd <matrixmdmd@gmail.com>
2017-05-30 16:37:39 -0700

    Added links to About and more things to examples

--- Weather App/app/components/About.jsx
+++ Weather App/app/components/About.jsx
@@ -3,8 +3,19 @@ var React = require('react');
 var About = (props) => {
     return (
         <div>
-            <h3>About</h3>
-            <p>Welcome to the About page!</p>
+            <h1 className="text-center">About</h1>
+            <p>This is my first React app! It's a weather app that takes locations and spits out the tempurature. Given that I've never learned html or javascript before I think it's prettttyy good!</p>
+            <ol>
+                <li>
+                    <a href="https://github.com/MichaelHDing/React">GitHub</a> - Take a look at the code!
+                </li>
+                <li>
+                    <a href="https://dashboard.heroku.com/apps/hidden-hollows-92583">Heroku</a> - Something!
+                </li>
+                <li>
+                    <a href="http://openweathermap.org/">OpenWeatherMap</a> - Where I get the weather data from!
+                </li>
+            </ol>
         </div>
     );
 };
