be2147c2242c499554a2040bec2db0d17da9a6f2
Matrixmdmd <matrixmdmd@gmail.com>
2017-05-31 10:33:16 -0700

    Finished and new Template

--- Template/webpack.config.js
+++ Template/webpack.config.js
@@ -1,5 +1,20 @@
+var webpack = require('webpack');
+
 module.exports = {
-    entry:'./app/app.jsx',
+    entry:[
+        'script!jquery/dist/jquery.min.js',
+        'script!foundation-sites/dist/foundation.min.js',
+        './app/app.jsx'
+    ],
+    externals: {
+        jquery: 'jQuery'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            '$': 'jquery',
+            'jQuery': 'jquery'
+        })
+    ],
     output:{
         path: __dirname,
         filename: './public/bundle.js'
@@ -8,6 +23,8 @@ module.exports = {
         root: __dirname,
         extensions: ['', '.js', '.jsx'],
         alias: {
+            Main: 'app/components/Main.jsx',
+            applicationStyles: 'app/styles/app.scss'
         }
     },
     module: {
@@ -21,6 +38,7 @@ module.exports = {
                 exclude: /(node_modules|bower_components)/
             }
         ]
-    }
+    },
+    devtool: 'cheap-module-eval-source-map'
 
 };
